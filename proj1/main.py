0cc0e3969cb3ae9a5e9146636f88d86f5d71c7b8
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-02-17 13:11:12 -0500

    formatting and preprocessing

--- proj1/main.py
+++ proj1/main.py
@@ -6,27 +6,30 @@ import pprint
 import sys
 import utils
 
+
 def main():
-	# Build a service object for interacting with the API. Visit
-	# the Google APIs Console <http://code.google.com/apis/console>
-	# to get an API key for your own application.
+    # Build a service object for interacting with the API. Visit
+    # the Google APIs Console <http://code.google.com/apis/console>
+    # to get an API key for your own application.
 
-	# !!! is invoking the program with python3 xx.py ok? or how do you get it to run with the ./run thing?
-	if len(sys.argv) != 5:
-		print("Usage: python3 main.py <google api key> <google engine id> <precision> <query>")
-		sys.exit(-1)
-	
-	dev_key = sys.argv[1]
-	search_engine_id = sys.argv[2]
-	desired_precision = sys.argv[3]
-	query = sys.argv[4]
+    # !!! is invoking the program with python3 xx.py ok? or how do you get it to run with the ./run thing?
+    if len(sys.argv) != 5:
+        print(
+            "Usage: python3 main.py <google api key> <google engine id> <precision> <query>"
+        )
+        sys.exit(-1)
 
-	cur_precision = INT_MIN
-	
-	while cur_precision < desired_precision:
-		res = utils.getQueryResult(dev_key, search_engine_id, query, desired_precision)
-		relevant_docs = utils.getRelevanceFeedback(res)
+    dev_key = sys.argv[1]
+    search_engine_id = sys.argv[2]
+    desired_precision = float(sys.argv[3])
+    query = sys.argv[4]
+
+    cur_precision = -1
+
+    while cur_precision < desired_precision:
+        res = utils.getQueryResult(dev_key, search_engine_id, query, desired_precision)
+        relevant_docs = utils.getRelevanceFeedback(res)
 
 
 if __name__ == "__main__":
-	main()
+    main()
