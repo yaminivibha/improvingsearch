8ce639af99569eaf9ff6b5f9e5edd2da5fb983a7
erl-ang <ell2147@columbia.edu>
2023-02-20 05:28:38 +0000

    sklearn works now ig

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo apt-get install python3-pip
 sudo apt-get install python3.7
 sudo update-alternatives --install /usr/bin/python3 python3 /usr/bin/python3.7 1
 sudo -H pip3 install --upgrade pip
-sudo pip3 install --upgrade google-api-python-client
+pip3 install --upgrade google-api-python-client
 sudo apt-get install python-setuptools python-dev build-essential
 sudo pip install -r requirements.txt
+pip3 install -U scikit-learn
